fb9b54d49a17715e5fb4c68fcdf93d2e3b3ecfe4
Onkar7666 <150422828+Onkar7666@users.noreply.github.com>
2024-09-01 23:59:58 +0530

    my first  js porg

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+//primitive
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt('5')+BigInt('5');
+let e = 'harry';
+let f = Symbol("I am Gammer");
+let G 
+console.log(a,b,c,d,e,f,G);
+
+console.log(typeof f);
+
+//non primitiver
+
+const house = {
+    Bhk : 2,
+    color : 'white',
+    add : 'pune',
+    price : 80000000,
+}
+console.log(house);
+
+
